cdc985c8436d149fb8a52eeb61e3f6d3d1fac080
Robert Morris <rtm@csail.mit.edu>
2020-09-21 12:25:07 -0400

    cosmetic changes

--- ft8mon.cc
+++ ft8mon.cc
@@ -18,16 +18,7 @@
 #include "snd.h"
 #include "util.h"
 #include "unpack.h"
-
-extern "C" {
-  typedef int (*cb_t)(int *a91, double hz0, double hz1, double off,
-                      const char *, double snr);
-  void entry(double xsamples[], int nsamples, int start, int rate,
-             double min_hz,
-             double max_hz,
-             int hints1[], int hints2[], double time_left,
-             double total_time_left, cb_t cb);
-};
+#include "ft8.h"
 
 std::mutex cycle_mu;
 volatile int cycle_count;
@@ -39,7 +30,7 @@ std::map<std::string,bool> cycle_already;
 //
 int
 hcb(int *a91, double hz0, double hz1, double off,
-    const char *comment, double snr)
+    const char *comment, double snr, int pass)
 {
   std::string msg = unpack(a91);
 
@@ -64,7 +55,7 @@ hcb(int *a91, double hz0, double hz1, double off,
          result.tm_min,
          result.tm_sec,
          (int)snr,
-         off - 1.5,
+         off - 0.5,
          (int)hz0,
          msg.c_str());
   fflush(stdout);
@@ -77,11 +68,11 @@ usage()
 {
   fprintf(stderr, "Usage: ft8mon -card card channel\n");
 #ifdef AIRSPYHF
-  fprintf(stderr, "       ft8mon -card airspy mhz\n");
+  fprintf(stderr, "       ft8mon -card airspy serial,mhz\n");
 #endif
   fprintf(stderr, "       ft8mon -levels card channel\n");
   fprintf(stderr, "       ft8mon -list\n");
-  fprintf(stderr, "       ft8mon -file xxx.wav\n");
+  fprintf(stderr, "       ft8mon -file xxx.wav ...\n");
   exit(1);
 }
 
@@ -91,8 +82,6 @@ main(int argc, char *argv[])
   int hints[2] = { 2, 0 }; // CQ
   double budget = 5; // compute for this many seconds per cycle
 
-  // avoid snd card input overruns.
-  // it's not clear why there's a problem.
   extern int fftw_type;
   fftw_type = FFTW_ESTIMATE; // rather than FFTW_MEASURE
 
@@ -111,16 +100,33 @@ main(int argc, char *argv[])
 
       if(tt - cycle_start >= 14){
         double ttt_start;
-        // get all waiting samples.
-        // ttt is UNIX time of samples[0].
-        std::vector<double> samples = sin->get(rate * 100, ttt_start);
+        // asking for no more than 15 seconds of samples in order
+        // to avoid missing in fftw plan cache.
+        // the "1" asks for the most recent 15 seconds of samples,
+        // not the oldest buffered. it causes samples before the
+        // most recent 15 seconds to be discarded.
+        std::vector<double> samples = sin->get(15 * rate, ttt_start, 1);
 
+        // ttt_start is UNIX time of samples[0].
         double ttt_end = ttt_start + samples.size() * (1.0 / rate);
         cycle_start = ((long long) (ttt_end / 15)) * 15;
 
-        long long nominal = samples.size() - rate * (ttt_end - cycle_start - 0.5);
-        if(nominal >= 0){
-          printf("decodes: %d\n", cycle_count);
+        // sample # of 0.5 seconds into the 15-second cycle.
+        long long nominal_start = samples.size() - rate * (ttt_end - cycle_start - 0.5);
+
+        if(nominal_start >= 0 && nominal_start + 10*rate < samples.size()){
+          struct tm result;
+          time_t tx = cycle_start;
+          gmtime_r(&tx, &result);
+          printf("%02d:%02d:%02d decodes: %d\n",
+                 result.tm_hour,
+                 result.tm_min,
+                 result.tm_sec,
+                 cycle_count);
+
+          // make samples exactly 15 seconds, to make
+          // fftw plan caching more effective.
+          samples.resize(15 * rate, 0.0);
 
           cycle_mu.lock();
           cycle_count = 0;
@@ -128,7 +134,7 @@ main(int argc, char *argv[])
           cycle_already.clear();
           cycle_mu.unlock();
 
-          entry(samples.data(), samples.size(), nominal, rate,
+          entry(samples.data(), samples.size(), nominal_start, rate,
                 150,
                 2900,
                 hints, hints, budget, budget, hcb);
@@ -142,14 +148,16 @@ main(int argc, char *argv[])
     SoundIn *sin = SoundIn::open(argv[2], argv[3]);
     sin->start();
     sin->levels();
-  } else if(argc == 3 && strcmp(argv[1], "-file") == 0){
-    // the .wav file should start at an even 15-second boundary.
-    int rate;
-    std::vector<double> s = readwav(argv[2], rate);
-    entry(s.data(), s.size(), 0.5 * rate, rate,
-          150,
-          2900,
-          hints, hints, budget, budget, hcb);
+  } else if(argc >= 3 && strcmp(argv[1], "-file") == 0){
+    for(int ii = 2; ii < argc; ii++){
+      // the .wav file should start at an even 15-second boundary.
+      int rate;
+      std::vector<double> s = readwav(argv[ii], rate);
+      entry(s.data(), s.size(), 0.5 * rate, rate,
+            150,
+            2900,
+            hints, hints, budget, budget, hcb);
+    }
   } else if(argc == 2 && strcmp(argv[1], "-list") == 0){
     snd_list();
   } else {
